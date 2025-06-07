e428d3e0bc50064d130d231a72a49e71855249c4
ampelme <olajohnfilhmar@gmail.com>
2025-06-07 23:36:59 +0800

    new grouped routes for handling admin requests

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->group('admin', function($routes) {
+  $routes->get('/', 'Admin::index');
+  $routes->get('results', 'Admin::results');
+  $routes->get('candidates', 'Admin::candidates');
+  $routes->get('student-lists', 'Admin::studentLists');
+  $routes->post('campaigns/add', 'Admin::addCampaign');
+});
 $routes->get('/login', 'Home::login');
-$routes->get('/admin', 'Admin::index');
