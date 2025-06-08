b661c097858b5db6d2a6b152c3c9e4da0c978503
ampelme <olajohnfilhmar@gmail.com>
2025-06-09 05:26:00 +0800

    added: added datatable cdn css and scripts

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,1 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.tailwindcss.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
