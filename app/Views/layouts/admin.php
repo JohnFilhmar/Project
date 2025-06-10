7369cb14197ada3d7077fbdab33c416f97a6d916
ampelme <olajohnfilhmar@gmail.com>
2025-06-10 09:32:13 +0800

    style: add full height to admin main section for improved layout

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -6,7 +6,7 @@
 <body>
   <div class="relative flex flex-col min-h-screen text-green-900">
     <?= view('partials/headers') ?>
-    <main class="min-h-screen overflow-y-auto p-4 pt-20 md:pt-25 lg:pt-30 main-bg">
+    <main class="min-h-screen overflow-y-auto p-4 pt-20 md:pt-25 lg:pt-30 main-bg h-full">
       <div class="flex flex-col items-center justify-center h-full w-full text-center">
         <?= $this->renderSection('content') ?>
       </div>
