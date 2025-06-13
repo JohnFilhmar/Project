6e6a3efdb1d1c29d92c8da44983fd36e8ad649c6
Filhmar - IT OJT <olajohnfilhmar@gmail.com>
2025-06-13 10:23:20 +0800

    fix: update notification handling in footer to differentiate success and error messages

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -2,8 +2,13 @@
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/dataTables.tailwindcss.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-<?php if (session()->has('notif')): ?>
-  <div id="notification" class="fixed bottom-5 right-5 bg-green-600 text-white px-4 py-2 rounded shadow-md z-50 transition-opacity duration-500 ease-in-out">
-    <?= session('notif') ?>
+<?php if (session()->has('notif-success')): ?>
+  <div id="notification" class="fixed bottom-5 right-5 bg-green-600 text-white px-4 py-2 rounded shadow-md z-50 transition-opacity duration-500 ease-in-out text-2xl">
+    <?= session('notif-success') ?>
+  </div>
+<?php endif; ?>
+<?php if (session()->has('notif-error')): ?>
+  <div id="notification" class="fixed bottom-5 right-5 bg-red-600 text-white px-4 py-2 rounded shadow-md z-50 transition-opacity duration-500 ease-in-out text-2xl">
+    <?= session('notif-error') ?>
   </div>
 <?php endif; ?>
